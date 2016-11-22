878ad9e78c0b050284373f5b3b3432c0fa70c676
Короткова Юлия Владимировна <y.korotkova@t1-group.ru>
2016-11-22 14:04:42 +0300

    Задание 3. Сделайте сценарий логина

---
+++ adminLogin/src/test/java/adminLoginTest.java
@@ -0,0 +1,41 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by korotkovay on 22.11.2016.
+ */
+public class adminLoginTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void testAdminLogin() {
+        driver.navigate().to("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        driver.findElement(By.xpath("//a[@title=\"Logout\"]")).click();
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+}
