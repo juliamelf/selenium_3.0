797ed6347b06ce8a1f6b92f89f8e545f76570ee8
Короткова Юлия Владимировна <y.korotkova@t1-group.ru>
2016-12-12 21:20:57 +0300

    add product

--- adminTest/src/test/java/tests/adminAddProductTest.java
+++ adminTest/src/test/java/tests/adminAddProductTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import testBase.TestBase;
@@ -23,6 +24,69 @@ public class adminAddProductTest extends TestBase{
 
         //add new product
         driver.findElement(By.xpath("//a[contains(@href,'edit_product')]")).click();
+
+        //Status
+        driver.findElement(By.cssSelector("label>input[value='1']")).click();
+
+        //Name
+        int productId = (int)(Math.random()*100);
+        String productName = "Test Product" + productId;
+        driver.findElement(By.name("name[en]")).sendKeys(productName);
+
+        //Code
+        driver.findElement(By.name("code")).sendKeys("test 123");
+
+        //Categories
+        driver.findElement(By.cssSelector("td>input[value='1']")).click();
+
+        //Default Category
+        Assert.assertEquals("Rubber Ducks", driver.findElement(By.name("default_category_id")).getText());
+
+        //Product Groups
+        driver.findElement(By.cssSelector("td>input[value='1-3']")).click();
+
+        //Quantity
+        driver.findElement(By.name("quantity")).clear();
+        driver.findElement(By.name("quantity")).sendKeys("25");
+
+        //Quantity Unit
+
+        //Delivery Status
+
+        //Sold Out Status
+        driver.findElement(By.name("sold_out_status_id")).click();
+        driver.findElement(By.cssSelector("select[name='sold_out_status_id']>option[value='2']")).click();
+
+        //Upload Image
+
+        //Date Valid From
+
+        //Date Valid To
+
+        //Manufacturer
+
+        //Supplier
+
+        //Keywords
+
+        //Short Description
+
+        //Description
+
+        //Head Title
+
+        //Meta Description
+
+        //Purchase Price
+
+        //Tax Class
+
+        //Price USD
+
+        //Price EUR
+
+        driver.findElement(By.name("save")).click();
+
 
 
 
