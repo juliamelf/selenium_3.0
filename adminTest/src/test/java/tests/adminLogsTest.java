cd13d04dc5aa789274ddc2fb64ca63a347f7de7a
Короткова Юлия Владимировна <y.korotkova@t1-group.ru>
2016-12-21 21:46:53 +0300

    Задание 17. Проверьте отсутствие сообщений в логе браузера

--- adminTest/src/test/java/tests/adminLogsTest.java
+++ adminTest/src/test/java/tests/adminLogsTest.java
@@ -15,20 +15,24 @@ public class adminLogsTest extends TestBase {
     @Test
     public void testBrowserLogs() throws InterruptedException {
 
+        //Login to Admin
         driver.navigate().to("http://localhost/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
 
+        //Open all products
         String category = "http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1";
         driver.navigate().to(category);
         List<WebElement> products = driver.findElements(By.cssSelector("img + a"));
         for (int i = 0; i < products.size(); i++){
             List<WebElement> productLinks = driver.findElements(By.cssSelector("img + a"));
             productLinks.get(i).click();
-            driver.manage().logs().get("browser").getAll().forEach(System.out::println);
             driver.findElement(By.name("cancel")).click();
         }
+
+        //Get logs
+        driver.manage().logs().get("browser").getAll().forEach(System.out::println);
 
     }
 
